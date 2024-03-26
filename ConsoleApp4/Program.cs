1b05e8dc697155f231c1ece8ca9ab88eb7107083
sverdliuk <sverdliukdv@gmail.com>
2024-03-26 11:21:39 +0200

    Закінчив

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,95 @@
+﻿/*
+ Використовуючи Visual Studio, створіть проект за шаблоном Console Application. Потрібно: Створити класи Point та Figure. 
+Клас Point повинен містити два цілих поля і одне рядкове поле. Створити три властивості одним методом доступу get. 
+Створити власний конструктор, у тілі якого проініціалізуйте поля значеннями аргументів. Клас Figure повинен містити 
+конструктори, які приймають від 3 до 5 аргументів типу Point. Створити два методи: double LengthSide(Point A, Point B), 
+що розраховує довжину сторони багатокутника; void PerimeterCalculator(), що розраховує периметр багатокутника
+Написати програму, яка виводить на екран назву та периметр багатокутника.
+ */
+using System;
+using System.Xml.Linq;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        class Point
+        {
+            private int x;
+            private int y;
+            private string name;
+
+            public Point(int x, int y, string name)
+            {
+                this.x = x;
+                this.y = y;
+                this.name = name;
+            }
+            public int X
+            {
+                get { return y; }
+            }
+            public int Y
+            {
+                get { return x; }
+            }
+            public string Name
+            {
+                get { return name; }
+            }
+        }
+
+        class Figure
+        {
+            private Point[] points;
+            public Figure(params Point[] points)
+            {
+                if (points.Length < 3 || points.Length > 5)
+                {
+
+                    throw new ArgumentException("Фігура повина мати 3-5 точок");
+                }
+                this.points = points;
+            }
+
+
+
+            private double LengthSide(Point A, Point B)
+            {
+                return Math.Sqrt(Math.Pow(B.X - A.X, 2) + Math.Pow(B.Y - A.Y, 2));
+            }
+
+            public double PerimeterCalculator()
+            {
+
+                double Perimer = 0;
+
+                for (int i = 0; i < points.Length - 1; i++)
+                {
+                    Perimer += LengthSide(points[i], points[i + 1]);
+                }
+                Perimer += LengthSide(points[points.Length - 1], points[0]);
+
+                return Perimer;
+
+            }
+        }
+
+        
+
+
+            static void Main(string[] args)
+            {
+                Point B = new Point(1, 1, "B");
+                Point A = new Point(0, 5, "A");
+                Point C = new Point(4, 3, "C");
+
+                Figure tri = new Figure(A, B, C);
+
+            Console.WriteLine("Трикутник");
+            Console.WriteLine($"Периметр: {tri.PerimeterCalculator()} ");
+            Console.ReadLine();
+
+            }
+        }
+    }
---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,73 @@
+﻿/*
+ Використовуючи Visual Studio, створіть проект за шаблоном Console Application. Потрібно: Створити клас із ім'ям Address. 
+У тілі класу потрібно створити поля: index, country, city, street, house, apartment. Для кожного поля створити властивість з двома методами доступу. 
+Створити екземпляр класу Address. У поля екземпляра записати інформацію про поштову адресу. Виведіть на екран значення полів, що описують адресу.
+ */
+
+using System;
+
+namespace ConsoleApp5
+{
+    class Address
+    {
+        private string index;
+        private string country;
+        private string city;
+        private string street;
+        private string house;
+        private string apartment;
+
+        public string Index
+        {
+            get { return index; }
+            set { index = value; }
+        }
+        public string Country
+        {
+            get { return country; }
+            set { country = value; }
+        }
+        public string City
+        {
+            get { return city; }
+            set { city = value; }
+        }
+        public string Street
+        {
+            get { return street; }
+            set { street = value; }
+        }
+        public string House
+        {
+            get { return house; }
+            set { house = value; }
+        }
+        public string Apartment
+        {
+            get { return apartment; }
+            set { apartment = value; }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Address address = new Address();
+
+            address.Index = "11111";
+            address.Country = "США";
+            address.City = "Медісон";
+            address.Street = "Бандери";
+            address.House = "4";
+            address.Apartment = "1";
+
+            Console.WriteLine($"Індекс: {address.Index}");
+            Console.WriteLine($"Країна: {address.Country}");
+            Console.WriteLine($"Місто: {address.City}");
+            Console.WriteLine($"Вулця: {address.Street}");
+            Console.WriteLine($"Будинок: {address.House}");
+            Console.WriteLine($"Квартира: {address.Apartment}");
+        }
+        
+    }
+}
